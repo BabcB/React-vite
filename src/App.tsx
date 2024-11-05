5824549b90f34b49b566000bf39ad8e5f5049247
BabcB <85190284+BabcB@users.noreply.github.com>
2024-11-05 14:20:50 +0000

    remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="card">
+    <div>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
